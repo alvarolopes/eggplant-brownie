abe40976404b503740952977b4bd3169dcbd5db5
alvaro <alvaroalberto@gmail.com>
2015-07-15 16:14:15 +0100

    Começando com swift

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var nameField:UITextField!
+    @IBOutlet var happinessField:UITextField!
+    
+    @IBAction func add(){
+        
+        println("eaten a \(nameField.text), \(happinessField.text) stars!.");
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
